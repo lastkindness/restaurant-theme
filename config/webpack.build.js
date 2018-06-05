2458cf9f0995afdd7ab27f5ab6a5e488b1f382c9
lastkindness <lastkindnesss@gmail.com>
2018-06-05 21:51:12 +0300

    add header #1

--- config/webpack.build.js
+++ config/webpack.build.js
@@ -1,10 +1,10 @@
-import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
+/*import UglifyJSPlugin from 'uglifyjs-webpack-plugin';*/
 import CleanWebpackPlugin from 'clean-webpack-plugin';
 import { DIRECTION } from './settings';
 
 export default {
     plugins: [
-        new UglifyJSPlugin(),
+        /*new UglifyJSPlugin(),*/
         new CleanWebpackPlugin('dist', {
             root: DIRECTION
         })
