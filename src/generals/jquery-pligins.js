f9c3c0031d2598f6b8fd3c1222c88bd8a9c5a7c1
lastkindness <lastkindnesss@gmail.com>
2018-06-15 15:11:53 +0300

    clean theme

--- src/generals/jquery-pligins.js
+++
@@ -1,7 +0,0 @@
-// import $ from 'jquery';
-// import 'slick-carousel';
-// import 'jquery-mask-plugin';
-// import 'jquery-ui/ui/core';
-// import 'jquery-ui/ui/widgets/slider';
-
-// export default $
--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import './style.scss';
 
- import Trigger from './generals/trigger';
-
- import LanguageSwitch from './components/language-switch/language-switch';
 
 document.addEventListener('DOMContentLoaded', () => {
-     new Trigger();
-     new LanguageSwitch();
+
 });
