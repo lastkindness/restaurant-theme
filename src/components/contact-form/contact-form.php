9badb56ee4cbf88798677c57a31d252362069511
SkyLight <kopel@aport.ru>
2018-08-16 02:19:18 +0300

    contacts-2 page

---
+++ src/components/contact-form/contact-form.php
@@ -0,0 +1,24 @@
+<form action="https://2017.wordpress.net/wp-comments-post.php" method="post" id="commentform" class="comment-form" novalidate="">
+    <p class="comment-form-author">
+        <input id="author" name="author" type="text" value="" size="47" maxlength="367" required="required"
+               placeholder="Name">
+    </p>
+    <p class="comment-form-phone">
+        <input id="author" name="phone" type="text" value="" size="47" maxlength="367" required="required"
+               placeholder="Phone">
+    </p>
+    <p class="comment-form-email">
+        <input id="email" name="email" type="email" value="" size="47" maxlength="367" aria-describedby="email-notes"
+               required="required" placeholder="Email (optional)">
+    </p>
+    <p class="comment-form-comment">
+                <textarea id="comment" name="comment" cols="49" rows="8" maxlength="367" required="required"
+                          placeholder="Your wishes...">
+                </textarea>
+    </p>
+    <p class="form-submit">
+        <input name="submit" type="submit" id="submit" class="submit" value="Submit" hidden>
+        <input type="hidden" name="comment_post_ID" value="44" id="comment_post_ID">
+        <input type="hidden" name="comment_parent" id="comment_parent" value="0">
+    </p>
+</form>
